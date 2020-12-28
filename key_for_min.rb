b62ca95360f800bfdf21b9587d91661f0a783a54
John Fuquene <jfuquene@email.wm.edu>
2020-12-28 02:46:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  values =  name_hash.values
+  values = name_hash.values
   name_hash.keys[values.index(values.min).to_i]
 end
 
